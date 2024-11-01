622d72d249175060aeb7f0d38a3eefa357c5668b
Lanmir <101045379+Lanmir@users.noreply.github.com>
2024-11-01 10:55:36 +0530

    Initial Commit

---
+++ AppStoreJSONAPIs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreJSONAPIs
+//
+//  Created by Varshaa vasundra sivakumar on 01/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
